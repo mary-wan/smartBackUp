e1845381bda039315917646853e3fc77b8b3086d
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-12-20 17:49:18 +0300

    change setting

--- smartproject/settings.py
+++ smartproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5(7e3h=iyvaogohugu^n(4me#&e3%k0pke!davy^_2&#!oo-%%'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
