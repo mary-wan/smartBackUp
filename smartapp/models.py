68dc0eab25b74568a517696ccba59105c1af36f0
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2022-01-06 01:19:22 +0300

    add profile class

--- smartapp/models.py
+++ smartapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime as dt
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Profile(models.Model):
+  profile_pic = CloudinaryField("image")
+  bio = models.TextField()
+  contact=models.CharField(max_length=100)
+  user = models.OneToOneField(User,on_delete = models.CASCADE,null=True)
+  
+def save_profile(self):
+    self.save()
+
+def delete_profile(self):
+     self.save()
+
+def update_profile(self):
+     self.save() 
+
+@classmethod
+def filter_by_id(cls, id):
+        profile = Profile.objects.filter(user=id).first()
+        return profile
+
+def _str_(self):
+        return self.user.username
