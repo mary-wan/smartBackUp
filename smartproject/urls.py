e8b4916c1d326904f979795d5bd5db93bcad789b
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2022-01-06 01:25:18 +0300

    add paths in project

--- smartproject/urls.py
+++ smartproject/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django_registration.backends.one_step.views import RegistrationView
+from django.contrib.auth import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('smartapp.urls')),
+    path('accounts/register/', RegistrationView.as_view(success_url='/create_profile'),
+    name='django_registration_register'),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('logout/', views.logout_then_login, name='logout'),
+   
 ]
