046ca3fcd20c54d803a94706fbd33bae972495c5
mwendaB <brian.mwenda@student.moringaschool.com>
2022-01-06 12:12:59 +0300

    edit the models class

--- expenses/models.py
+++ expenses/models.py
@@ -12,3 +12,18 @@ class Expense(models.Model):
     owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
     category = models.CharField(max_length=266)
 
+    def __str__(self):
+        return self.category
+
+    class Meta:
+        ordering: ['-date']
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
