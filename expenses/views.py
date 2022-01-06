04def6ff30765948b6beb39c3a9e3cba54cb07f3
mwendaB <brian.mwenda@student.moringaschool.com>
2022-01-06 12:15:44 +0300

    add functionality

--- expenses/views.py
+++ expenses/views.py
@@ -38,3 +38,34 @@ def index(request):
     }
     return render(request, 'expenses/index.html', context)
 
+
+@login_required(login_url='/authentication/login')
+def add_expense(request):
+    categories = Category.objects.all()
+    context = {
+        'categories': categories,
+        'values': request.POST
+    }
+    if request.method == 'GET':
+        return render(request, 'expenses/add_expense.html', context)
+
+    if request.method == 'POST':
+        amount = request.POST['amount']
+
+        if not amount:
+            messages.error(request, 'Amount is required')
+            return render(request, 'expenses/add_expense.html', context)
+        description = request.POST['description']
+        date = request.POST['expense_date']
+        category = request.POST['category']
+
+        if not description:
+            messages.error(request, 'description is required')
+            return render(request, 'expenses/add_expense.html', context)
+
+        Expense.objects.create(owner=request.user, amount=amount, date=date,
+                               category=category, description=description)
+        messages.success(request, 'Expense saved successfully')
+
+        return redirect('expenses')
+
