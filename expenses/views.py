92366018a742e0cafe4292ef14736878afe56a9d
mwendaB <brian.mwenda@student.moringaschool.com>
2022-01-06 12:14:56 +0300

    add the views section

--- expenses/views.py
+++ expenses/views.py
@@ -11,3 +11,30 @@ from userpreferences.models import UserPreference
 import datetime
 
 
+def search_expenses(request):
+    if request.method == 'POST':
+        search_str = json.loads(request.body).get('searchText')
+        expenses = Expense.objects.filter(
+            amount__istartswith=search_str, owner=request.user) | Expense.objects.filter(
+            date__istartswith=search_str, owner=request.user) | Expense.objects.filter(
+            description__icontains=search_str, owner=request.user) | Expense.objects.filter(
+            category__icontains=search_str, owner=request.user)
+        data = expenses.values()
+        return JsonResponse(list(data), safe=False)
+
+
+@login_required(login_url='/authentication/login')
+def index(request):
+    categories = Category.objects.all()
+    expenses = Expense.objects.filter(owner=request.user)
+    paginator = Paginator(expenses, 5)
+    page_number = request.GET.get('page')
+    page_obj = Paginator.get_page(paginator, page_number)
+    currency = UserPreference.objects.get(user=request.user).currency
+    context = {
+        'expenses': expenses,
+        'page_obj': page_obj,
+        'currency': currency
+    }
+    return render(request, 'expenses/index.html', context)
+
