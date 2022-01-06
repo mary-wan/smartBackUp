b8dec6fb7b7f57ea26d70f8194b9771548cfc5fd
mwendaB <brian.mwenda@student.moringaschool.com>
2022-01-06 12:12:26 +0300

    finish writing the models class

--- expenses/models.py
+++ expenses/models.py
@@ -5,3 +5,10 @@ from django.utils.timezone import now
 # Create your models here.
 
 
+class Expense(models.Model):
+    amount = models.FloatField()
+    date = models.DateField(default=now)
+    description = models.TextField()
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    category = models.CharField(max_length=266)
+
